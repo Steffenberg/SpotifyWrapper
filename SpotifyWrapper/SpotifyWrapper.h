9c6d2b6372a22c56f56248e1c73723eb406cecbf
Steffen Gruschka <s.gruschka@salt-and-pepper.eu>
2019-12-05 11:16:38 +0100

    Initial Commit

---
+++ SpotifyWrapper/SpotifyWrapper.h
@@ -0,0 +1,19 @@
+//
+//  SpotifyWrapper.h
+//  SpotifyWrapper
+//
+//  Created by Steffen Gruschka on 05.12.19.
+//  Copyright © 2019 Steffen Gruschka. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SpotifyWrapper.
+FOUNDATION_EXPORT double SpotifyWrapperVersionNumber;
+
+//! Project version string for SpotifyWrapper.
+FOUNDATION_EXPORT const unsigned char SpotifyWrapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SpotifyWrapper/PublicHeader.h>
+
+
